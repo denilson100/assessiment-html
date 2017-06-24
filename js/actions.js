6d0b8272e157036fd1c4ecad99c9e4ae4157328e
denilson100 <denilson100@gmail.com>
2017-06-24 12:25:08 -0300

    Segundo commit

--- js/actions.js
+++ js/actions.js
@@ -12,12 +12,19 @@
 // 	img[i] = new Image();
 // 	img[i].src = 'img/img' + i + '.png';
 // 	memory_array += img[i].src;
-// }
+//
+
 
 var memory_array = ['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8'];
 var memory_values = [];
 var memory_tile_ids = [];
 var tiles_flipped = 0;
+var jogadas = 0;
+
+var pause = 0;
+var count = 0;
+var counter = setInterval(timer, 1000);
+var stoped = 0
 
 function newBoard() {
 	tiles_flipped = 0;
@@ -25,12 +32,20 @@ function newBoard() {
 
   var output = '';
   _.forEach(memory_array, function(memory_array_value, index) {
-    // output += '<div id="tile_'+ index +'" onclick="memoryFlipTile(this,\''+ memory_array_value +'\')"></div>';
-		output += '<div id="tile_'+ index +'" onclick="memoryFlipTile(this,\''+ memory_array_value +'\')"><img src="img/img' + memory_array_value +'.png"></div>';
+    output += '<div id="tile_'+ index +'" onclick="memoryFlipTile(this,\''+ memory_array_value +'\')"></div>';
   });
 
 	document.getElementById('memory_board').innerHTML = output;
+	jogadas = 0;
+	document.getElementById('quantidadeJogadas').innerHTML = 'Jogadas: ' + jogadas;
+	mostraBotao();
+	ReStartFunction();
+
 }
+
+// function init() {
+// 	document.getElementById("div1").style.display = 'none';
+// }
 
 function canFlipCard(tile) {
   return tile.innerHTML == "" && memory_values.length < 2;
@@ -66,18 +81,85 @@ function isGameOver() {
 }
 
 function gameIsOver() {
-  alert("Board cleared... generating new board");
-  document.getElementById('memory_board').innerHTML = "";
-  newBoard();
+	// Apresenta estatus
+	mostraBotao();
+	PauseFunction();
+	// document.getElementById('quantidadeJogadas').innerHTML = 'Jogadas: ' + jogadas;
+	document.getElementById('tempo').innerHTML = 'Tempo: ' + tempo;
+	document.getElementById("div").style.display = 'block';
+
+}
+
+function mostraBotao() {
+        var display = document.getElementById("div").style.display;
+        if(display == "none")
+            document.getElementById("div").style.display = 'block';
+        else
+            document.getElementById("div").style.display = 'none';
+    }
+
+function timer() {
+  count = count+1;
+  document.getElementById("timer").innerHTML=count + " secs";
+	document.getElementById("div").style.display = 'none';
+}
+
+function StopFunction() {
+  clearInterval(counter);
+  window.count=0;
+  window.pause=0;
+  document.getElementById("pause").innerHTML="Pause"
+  window.stoped=1
+  document.getElementById("timer").innerHTML=count + " secs";
+}
+
+function ReStartFunction() {
+  if (counter) {
+    clearInterval(counter);
+    window.pause=0;
+    window.count=0;
+    window.stoped=0
+    window.counter=setInterval(timer, 1000);
+    count=count+1;
+    document.getElementById("pause").innerHTML="Pause"
+    document.getElementById("timer").innerHTML=count + " secs";
+
+  }
+}
+
+function PauseFunction() {
+  if (stoped==0) {
+    if (pause==0) {
+      clearInterval(counter);
+      document.getElementById("pause").innerHTML="Resume"
+      pause=1;
+      return;
+    }
+
+  if (pause==1) {
+      window.counter=setInterval(timer, 1000);
+      document.getElementById("timer").innerHTML=count + " secs";
+      document.getElementById("pause").innerHTML="Pause"
+      pause=0;
+      return;
+    }
+  }
+  return;
 }
 
 function cardsDoNotMatch() {
   setTimeout(flipCardBack, 700);
 }
 
+// Retorna quando clica
 function flipCard(tile, value) {
   tile.style.background = '#FFF';
-  tile.innerHTML = value;
+  tile.innerHTML = '<img src="img/img' + value +'.png" >';
+}
+
+function mostrarTodas(tile, value){
+	tile.style.background = '#FFF';
+	tile.innerHTML = '<img src="img/img' + value +'.png" >';
 }
 
 function flipCardBack() {
@@ -95,6 +177,8 @@ function flipCardBack() {
 }
 
 function memoryFlipTile(tile, value) {
+	jogadas += 1;
+	document.getElementById('quantidadeJogadas').innerHTML = 'Jogadas: ' + jogadas;
 	if (canFlipCard(tile)) {
 		flipCard(tile, value);
     if (areNoCardsFlipped()) {
